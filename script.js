2fcc39ba570a3ed207af140d9b58e96cb481b716
Rati Elizbarashvili <elizbarashvilirati15@Gmail.com>
2024-10-19 02:05:30 +0400

    done

---
+++ script.js
@@ -0,0 +1,16 @@
+const Person = {
+
+    firstname : "Rati",
+    lastname : "Elizbarashvili" ,
+    Gmail : "elizbarashvilirati15@Gmai.com" , 
+    Phone : "+995598250185",
+}
+let Personage = {
+    age : 19,
+    agechecked : true,
+}
+
+console.log(Person)
+
+console.log(typeof Person)
+console.log(Personage)
